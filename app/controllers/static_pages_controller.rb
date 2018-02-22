74ce7858051ecc56e1470afdc928164deed394b0
koltor <maxence.theme@gmail.com>
2018-02-22 14:03:17 +0100

    tous est bon

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
+ 		prng = Random.new
+		if prng.rand(100) >= 50
+			redirect_to home2_path
+		end
   end
 
   def index2
   end
-  
+
 end
