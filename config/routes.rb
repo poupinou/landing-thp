73e6c597ce1e7c51706ada002820842ff37eb442
koltor <maxence.theme@gmail.com>
2018-02-22 13:11:33 +0100

    ajout des 2 pages de formulaires

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#index', as: 'home'
 
+  get '/index' , to: 'static_pages#index2', as: 'home2'
+
   get 'test_mail', to: 'static_pages#test_mail'
 
   resources :static_pages
